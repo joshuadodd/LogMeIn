c0eac87fa922bb846396af5d4b7842e815057bde
Nathan Hosselton <nathanhosselton@gmail.com>
2017-10-21 13:01:58 -0400

    Initial Commit

---
+++ LogMeIn/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  LogMeIn
+//
+//  Created by Nathan Hosselton on 10/21/17.
+//  Copyright © 2017 Codebase. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
